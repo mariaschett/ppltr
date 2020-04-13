dc4b92a8d40d4aa59825a4134c00d991767e34b0
Julian Nagele <mail@jnagele.net>
2020-04-13 10:47:52 +0100

    Make peephole window size a command line argument, default to 6

--- main.ml
+++ main.ml
@@ -7,9 +7,10 @@ let () =
     [%map_open
       let bytecode_csv = anon ("BYTECODE" %: string)
       and blocks_fn = anon ("BLOCKS" %: string)
+      and peephole_sz = flag "peephole-size" (optional_with_default 6 int)
+          ~doc:"sz maximal size of the peephole window (default: 6)"
       in
       fun () ->
-        let peephole_sz = 6 in
         Csv.Rows.load ~has_header:true bytecode_csv
         |> List.map ~f:(fun r -> Csv.Row.find r "bytecode")
         |> Blk_generator.generate_blks peephole_sz
