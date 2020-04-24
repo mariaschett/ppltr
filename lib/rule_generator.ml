73db8827d689c444155e058df857e457a2ad1b60
Maria A Schett <mail@maria-a-schett.net>
2020-04-24 14:36:13 +0100

    Fix example and input/output

--- lib/rule_generator.ml
+++ lib/rule_generator.ml
@@ -5,6 +5,7 @@ open Ebso
 let in_header =
   [ "rule lhs"
   ; "rule rhs"
+  ; "vars"
   ; "gas saved"
   ; "optimization source"
   ; "optimization target"
@@ -34,8 +35,6 @@ let compute_multiple_optimizations rows =
   List.group ~break:(fun row1 row2 -> source row1 = source row2 && target row1 = target row2) rows
   |> List.filter ~f:(fun group -> List.length group > 1)
 
-let timed_out row = let rule = rule row in rule.lhs = []
-
 let insert_non_dup row_to_insert (rows, dups) =
   if List.exists rows ~f:(fun row  -> Rule.equal (rule row) (rule row_to_insert))
   then (rows, row_to_insert :: dups)
@@ -44,14 +43,16 @@ let insert_non_dup row_to_insert (rows, dups) =
 let rm_duplicates =
   List.fold ~init:([],[]) ~f:(fun (rows, dups) row -> insert_non_dup row (rows, dups))
 
+let timed_out row = Csv.Row.find row "rule lhs" = ""
+
 let compute_results in_csv =
-  let rows = Csv.Rows.load ~has_header:true ~header:Optz_generator.out_header in_csv in
+  let rows = Csv.Rows.load ~has_header:true ~header:in_header in_csv in
   (* remove timeouts from rows *)
-  let (timeouts, rows_without_timeouts) = List.partition_tf ~f:(timed_out) rows in
-  (* rows where optimizations gave rise to multiple rules *)
-  let multiples = compute_multiple_optimizations rows_without_timeouts in
+  let (timeouts, rows_without_timeouts) = List.partition_tf ~f:timed_out rows in
   (* remove duplciates *)
-  let (final_rows, duplicates) = rm_duplicates rows_without_timeouts
+  let (final_rows, duplicates) = rm_duplicates rows_without_timeouts in
+  (* rows where optimizations gave rise to multiple rules *)
+  let multiples = compute_multiple_optimizations final_rows
   in
   (final_rows, { duplicates = duplicates; multiples = multiples; timeouts = timeouts; })
 
@@ -59,30 +60,27 @@ let write_rules out_csv rules =
   let rules' = List.map ~f:(Csv.Row.to_list) rules in
   Csv.save out_csv (in_header :: rules')
 
+let print_stats key rows printer =
+  Format.printf "\nThe following %i %s:\n" (List.length rows) key;
+  List.iter rows ~f:printer;
+  Format.printf "\n"
+
 let print_dups stats =
-  Format.printf
-    "\nThe following %i duplicate rules were generated:\n"
-    (List.length stats.duplicates);
-  List.iter stats.duplicates ~f:(fun row -> Format.printf "%s" (Rule.show (rule row)))
+  print_stats "duplicate rules were generated"  stats.duplicates
+    (fun row -> Format.printf "%s" (Rule.show (rule row)))
 
 let show_optimization row =
   Printf.sprintf "%s >= %s" (Program.show_h (source row)) (Program.show_h (target row))
 
 let print_muls stats =
-  let print_mul group =
-    Format.printf "@[%s@\n" (show_optimization (List.hd_exn group));
-    List.iter group ~f:(fun row -> Format.printf "  %s@\n" (Rule.show (rule row)));
-    Format.printf "@]"
-  in
-  Format.printf "\nThe following %i optimizations generated multiple rules:"
-    (List.length stats.multiples);
-  List.iter stats.multiples ~f:print_mul
+  print_stats "optimizations generated multiple rules:" stats.multiples
+    (fun group ->
+       Format.printf "%s" (show_optimization (List.hd_exn group));
+       List.iter group ~f:(fun row -> Format.printf "\n  %s" (Rule.show (rule row))))
 
 let print_timeouts stats =
-  Format.printf
-    "\nFor following %i optimizations rule generation timed out:\n"
-    (List.length stats.timeouts);
-  List.iter stats.timeouts ~f:(fun row -> Format.printf "%s@\n" (show_optimization row))
+  print_stats "optimizations timed out while rule generation" stats.timeouts
+  (fun row -> Format.printf "%s@\n" (show_optimization row))
 
 let print_stats stats =
   print_dups stats;
